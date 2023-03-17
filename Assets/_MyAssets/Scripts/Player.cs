35e3c7e2c1718e7c078a9fe8bfd784f30f3cbffe
Fridriver <117280045+Fridriver@users.noreply.github.com>
2023-03-17 14:02:08 -0400

    Update Player.cs

--- Assets/_MyAssets/Scripts/Player.cs
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         // Position initiale du joueur
-        transform.position = new Vector3(-45f, 0.51f, -45f);  // place le joueur à sa position initiale 
+        transform.position = new Vector3(-45f, 3.51f, -45f);  // place le joueur à sa position initiale 
         _rb = GetComponent<Rigidbody>();  // Récupère le rigidbody du Player
     }
 
@@ -37,6 +37,8 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(positionX, 0f, positionZ);  // Établi la direction du vecteur à appliquer sur le joueur
         _rb.velocity = direction * Time.fixedDeltaTime * _vitesse;  // Applique la vélocité sur le corps du joueur dans la direction du vecteur
         // _rb.AddForce(direction * Time.fixedDeltaTime * _vitesse);  // Applique une force sur le joueur dans la direction du vecteur
+
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
 
         if (direction != Vector3.zero)
         {
